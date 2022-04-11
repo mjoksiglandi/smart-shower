53a94edfc804411e650fb955e421ca2d703fc4aa
Gormm <juan.cornejo.s@gmail.com>
2022-04-11 18:04:39 -0400

    add buzer

--- src/SmartShower.cpp
+++ src/SmartShower.cpp
@@ -18,6 +18,12 @@ DallasTemperature sensors(&oneWire);
 const  int Up = 25;
 const int Down = 27;
 const int Act = 13;
+const int ledPin = 5;
+bool sound;
+
+ int freq = 440;
+ int ledChannel = 0;
+ int resolution = 10;
 
 int VolSens;
 int LvlSens = 14;
@@ -27,6 +33,9 @@ int SolTank = 19;
 
 String Temperatura;
 int Mode = 0;
+
+int rep = 0;
+int alar = 0;
 
 struct volumen {
   String LLeno = "lleno";
@@ -64,10 +73,30 @@ struct Temp {
   Temp T;
   int SetTemp = 27;
 
+  void buzzer(int delayTime, int led, int intervalo) {
+    if (sound == false){
+    for (int lap = 0; intervalo >= lap; lap++){
+        ledcWrite(led, 50 );
+        delay(delayTime);
+        ledcWrite(led, 0);
+        delay(delayTime);
+        Serial.println(lap);
+        if (lap >= intervalo){
+          sound = true;
+      }
+    }
+  }
+    else{
+      ledcWrite(led, 0);
+    }
+  }
+
 void setup() {
   Serial.begin (115200);
   sensors.begin();
   u8g2.begin();
+
+  ledcSetup(ledChannel, freq, resolution);
 
   pinMode(VolSens, INPUT);
   pinMode(LvlSens, INPUT_PULLUP);
@@ -80,7 +109,10 @@ void setup() {
   pinMode(Down, INPUT_PULLUP);
   pinMode(Act, INPUT_PULLUP);
 
-  //Serial.begin(115200);
+  ledcAttachPin(ledPin, ledChannel);
+
+  Serial.begin(115200);
+  buzzer(50,  ledChannel, 3);
 }
 
 void loop() {
@@ -93,6 +125,8 @@ void loop() {
 
   u8g2.firstPage();
 
+  Serial.println(rep);
+
   if (Mode == 0){
     Status = estado.Inactivo;
     digitalWrite(SolShow, HIGH);
@@ -101,19 +135,28 @@ void loop() {
   if (button.Act == LOW){
     Mode = Mode + 1;
     delay (500);
+    sound = false;
+    buzzer(200,  ledChannel, 0);
   }
+
   if (T.ActualTemp < SetTemp && Mode != 0 && Estanque.full == false){
     digitalWrite(SolShow, LOW);
     digitalWrite(SolTank, HIGH);
     Status = estado.Esperando;
+    rep = 0;
   }
   if (T.ActualTemp >= SetTemp && Mode != 0 && Estanque.full == false){
     digitalWrite(SolShow, HIGH);
     digitalWrite(SolTank, LOW);
     Status = estado.Activo;
+    Serial.println(rep);
+    sound =false;
+    if (rep == 0){
+      buzzer(500,  ledChannel, 1);
+      rep = rep +1;
+    }
   }
-
-  if( Mode > 2 && button.Act == HIGH){
+  if( Mode >= 2 && button.Act == HIGH){
     Mode = 0;
     delay (500);
   }
@@ -122,20 +165,36 @@ void loop() {
     digitalWrite(SolTank, LOW);
     Estanque.full = true;
     Capacidad = Nivel.LLeno;
-  }
+    sound =false;
+
+    }
   else {
     Capacidad = Nivel.Vacio;
     Estanque.full = false;
+    alar = 0;
+  }
+  if(Estanque.full == true){
+    if (alar == 0){
+      buzzer(1000,  ledChannel, 3);
+      alar = alar +1;
+    }
+    // buzzer(1000,  ledChannel, 6);
   }
   if (T.ActualTemp < SetTemp && Mode != 0 && Estanque.full == true){
     digitalWrite(SolShow, LOW);
     digitalWrite(SolTank, LOW);
     Status = estado.Esperando;
+    rep = 0;
   }
   if (T.ActualTemp >= SetTemp && Mode != 0 && Estanque.full == true){
     digitalWrite(SolShow, LOW);
     digitalWrite(SolTank, LOW);
     Status = estado.Activo;
+    sound =false;
+    if (rep == 0){
+      buzzer(500,  ledChannel, 1);
+      rep = rep +1;
+    }
   }
   if (button.Up == LOW){
     SetTemp = SetTemp + 1;
@@ -161,7 +220,7 @@ void loop() {
     u8g2.drawStr(64,15,"T Act");
     u8g2.setCursor(94, 15);
     u8g2.print(T.ActualTemp);
-    u8g2.drawStr(112, 15,"C");
+    u8g2.drawStr(113, 15,"C");
     u8g2.setFont(u8g2_font_unifont_t_symbols);
     u8g2.drawGlyph(104, 15, 0x00b0);
     ///////////////////////////////
